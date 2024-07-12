02c99fcfd5f98a1993022f2a9c137828b04f5b17
Esme <esmepuzio@ucsb.edu>
2024-07-12 16:05:18 -0700

    README added.

---
+++ index.js
@@ -0,0 +1,4 @@
+export *  from '../src/coilCAM/function-operators/1D-functions';
+export *  from '../src/coilCAM/function-operators/2D-functions';
+export *  from '../src/coilCAM/function-operators/complex-functions';
+export *  from '../src/coilCAM/setup';
--- src/index.js
+++
@@ -1,4 +0,0 @@
-export *  from '../function-operators/1D-functions';
-export *  from '../function-operators/2D-functions';
-export *  from '../function-operators/complex-functions';
-export *  from '../setup';
