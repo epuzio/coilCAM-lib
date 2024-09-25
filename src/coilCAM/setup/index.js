0618481fe6358788fb271ead92ee006bfdb5a4e9
Esme <esmepuzio@ucsb.edu>
2024-09-24 18:01:26 -0700

    Removed TPV from library - OrbitControls exports causing errors when importing library

--- src/coilCAM/setup/index.js
+++ src/coilCAM/setup/index.js
@@ -4,7 +4,6 @@ import centerPrint from "./CenterPrint.js";
 import toolpathUnitGenerator from "./ToolpathUnitGenerator.js";
 import spiralize from "./Spiralize.js";
 import showCurve from "./ShowCurve.js";
-import ToolpathViewer from './ToolpathViewer.js';
 
 export {
     base, addBase, baseSpiral, baseFill, 
@@ -12,6 +11,5 @@ export {
     centerPrint,
     toolpathUnitGenerator, 
     spiralize, 
-    showCurve, 
-    ToolpathViewer
+    showCurve
 };
